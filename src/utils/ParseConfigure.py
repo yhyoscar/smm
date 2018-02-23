88ee03d6184c3446cc0b1f719f68aac2c8fc3cb4
Zhenqing Hu <huzq85@gmail.com>
2018-02-22 19:46:55 -0500

    Convert data in configure.py to json file, and add parse file to retrieve data from configure.json. This will separate the code and the configure data.
    Signed-off-by: Zhenqing Hu <huzq85@gmail.com>

---
+++ src/utils/ParseConfigure.py
@@ -0,0 +1,18 @@
+import os
+import json
+from pprint import pprint
+
+def get_confingure_data():
+    parent_path = os.path.abspath(os.path.dirname(os.getcwd()))
+    f_path = os.path.join(parent_path, "Configure\Configure.json")
+    with open(f_path) as data_file:
+        data = json.load(data_file)
+    pprint(data)
+    return data
+
+
+# if '__main__' == __name__:
+#     # parent_path = os.path.abspath(os.path.dirname(os.getcwd()))
+#     # f_path = os.path.join(parent_path, "Configure\Configure.json")
+#     # print (f_path)
+#     get_confingure_data()
